36e8c1a71460db0480994fc394008a855d67d983
Jeet Shah <jeetshah4321@gmail.com>
2017-08-07 23:50:16 -0400

    Added all functionality, no styling

---
+++ assets/js/todo.js
@@ -0,0 +1,20 @@
+//Strike-through on to-do with click
+$("ul").on("click","li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click X to delete to-do
+$("ul").on("click",".delete", function(event){
+    $(this).parent().slideUp(250,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//add new to-do on Enter
+$("input").keypress(function(event){
+    if(event.which === 13){
+        var newToDo = $(this).val();
+        $("ul").append("<li><span class='delete'>X</span> " + newToDo + "</li>");
+    }
+});
